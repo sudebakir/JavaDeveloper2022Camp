c9920dcd47f0a1c52f002f0420f75be1c8a25700
sudebakir <81747951+sudebakir@users.noreply.github.com>
2022-09-21 20:09:11 +0300

    Add files via upload

---
+++ Ders9devam.java
@@ -0,0 +1,31 @@
+
+public class Ders9devam {
+
+	public static void main(String[] args) {
+	
+		//for
+				for (int i=2; i<10; i+=2) {
+				
+				System.out.println(i);
+				
+				}
+
+				System.out.println("For Döngüsü Bitti");
+
+				int i=1;
+        //while
+				while (i<10) {
+					System.out.println(i);
+					i+=2;
+					
+				}
+			
+				
+				System.out.println("While Döngüsü Bitti");
+				
+		
+		
+
+	}
+
+}
