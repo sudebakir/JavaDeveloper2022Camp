2828c5ca4eaad0052126cd5e86bee17931998f46
sudebakir <81747951+sudebakir@users.noreply.github.com>
2022-09-21 18:52:04 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,23 @@
-package recapDemo1;
 
 public class Main {
 
 	public static void main(String[] args) {
-	
-		
-		int sayi1 =20;
-		int sayi2 =25;
-		int sayi3 =2;
-		
-			int enBuyuk= sayi1;
-			
-		
-		if(enBuyuk<sayi2) {
-			
-			enBuyuk=sayi2;
-			
-		}
-		
-		
-		if (enBuyuk<sayi3) {
-			
-			enBuyuk=sayi3;
-			
-		}
-		
-		System.out.println("En Buyuk = "+enBuyuk);
-		
-		
-		
-		
+		int ogrenciSayisi=12;
+		String mesaj="Öðrenci Sayýsý : ";
+		System.out.println(mesaj+ogrenciSayisi);
+		System.out.println(mesaj+ogrenciSayisi);
+		System.out.println("Öðrenci sayým : "+ ogrenciSayisi);
+		System.out.println("Öðrenci sayým : "+ ogrenciSayisi);
+		System.out.println("Öðrenci sayým : "+ ogrenciSayisi);
 
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
