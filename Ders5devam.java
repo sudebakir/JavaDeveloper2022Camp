cc9db9bf7bda5c380b8afcf3daffb00835e86d5f
sudebakir <81747951+sudebakir@users.noreply.github.com>
2022-09-21 18:54:38 +0300

    Add files via upload

---
+++ Ders5devam.java
@@ -0,0 +1,30 @@
+
+public class Ders5devam {
+
+	public static void main(String[] args) {
+		
+		int sayi1 =20;
+		int sayi2 =25;
+		int sayi3 =2;
+		
+			int enBuyuk= sayi1;
+			
+		
+		if(enBuyuk<sayi2) {
+			
+			enBuyuk=sayi2;
+			
+		}
+		
+		
+		if (enBuyuk<sayi3) {
+			
+			enBuyuk=sayi3;
+			
+		}
+		
+		System.out.println("En Buyuk = "+enBuyuk);
+
+	}
+
+}
