bbc424e89b4566161dbe6918d3e965ae82286799
sudebakir <81747951+sudebakir@users.noreply.github.com>
2022-09-21 18:38:42 +0300

    Add files via upload

---
+++ Ders3devam.java
@@ -0,0 +1,27 @@
+
+public class Ders3devam {
+
+	public static void main(String[] args) {
+		
+		int sayi=12;
+		
+		
+		
+		double sayi=12.5;
+		sayi=-129;
+		
+		char karakter="A";
+		boolean dogruMu=false;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
