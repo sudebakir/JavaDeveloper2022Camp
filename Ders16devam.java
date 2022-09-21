16c5bee5b8b724f3dc1c451e725a5a287877b44d
sudebakir <81747951+sudebakir@users.noreply.github.com>
2022-09-21 21:03:03 +0300

    Add files via upload

---
+++ Ders16devam.java
@@ -0,0 +1,48 @@
+
+public class Ders16devam {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+			char harf = 'E';
+			
+			switch(harf) 	{
+			case 'A':
+			case 'I':
+			case 'O':
+			case 'U':
+			
+			System.out.println("Kalýn sesli harf");
+			break;
+			default:
+				System.out.println("ince sesli harfler");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
