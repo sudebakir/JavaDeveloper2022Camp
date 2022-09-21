42f6d510c809ce3e7e331c861ae7a66d2b5ca018
sudebakir <81747951+sudebakir@users.noreply.github.com>
2022-09-21 20:59:16 +0300

    Add files via upload

---
+++ Ders15devam.java
@@ -0,0 +1,48 @@
+
+public class Ders15devam {
+
+	public static void main(String[] args) {
+		
+		//bug
+		int number=2;
+		int remainder =number %2;
+		System.out.println(remainder);
+		
+		boolean isPrime= true;
+		if(number==1) {
+			System.out.println("sayı asal değildir");
+			return;
+		}
+		
+		if(number<1) {
+			System.out.println("geçersiz sayı");
+		}
+		
+		for (int i=2; i<number;i++) {
+			if(number %i==0) {
+				isPrime=false;
+				
+			}
+		}
+		
+		if(isPrime ) {
+			System.out.println("Sayı asaldır");
+		}
+		
+		else {
+			System.out.println("sayı asal değildir");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
