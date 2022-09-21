9347bd2d31255e49be978b74f92c474c43b3c996
sudebakir <81747951+sudebakir@users.noreply.github.com>
2022-09-21 18:44:50 +0300

    Add files via upload

---
+++ Ders4devam.java
@@ -0,0 +1,27 @@
+
+public class Ders4devam {
+
+	public static void main(String[] args) {
+		
+		int sayi=24;
+		if(sayi<20) {
+			System.out.println("Sayý 20'den küçüktür");
+		}
+		
+		else if (sayi==20) {
+			System.out.println("Sayý 20'ye eþittir.");
+			
+		}
+		
+		else {
+			System.out.println("Sayý 20'den büyüktür.");
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
