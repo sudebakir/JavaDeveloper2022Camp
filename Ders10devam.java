ff2ba9ad892dfcf90cd21ea8a9bc12c8bcea0365
sudebakir <81747951+sudebakir@users.noreply.github.com>
2022-09-21 20:15:56 +0300

    Add files via upload

---
+++ Ders10devam.java
@@ -0,0 +1,49 @@
+
+public class Ders10devam {
+
+	public static void main(String[] args) {
+	
+		
+		//for
+		for (int i=2; i<10; i+=2) {
+		
+		System.out.println(i);
+		
+		}
+
+		System.out.println("For Döngüsü Bitti");
+
+		int i=1;
+//while
+		while (i<10) {
+			System.out.println(i);
+			i+=2;
+			
+		}
+	
+		
+		System.out.println("While Döngüsü Bitti");
+		
+		//do-while
+		
+		int j=100;
+		do {
+			System.out.println(j);
+			j+=2;
+			
+		}
+		
+		while (j<10) ;
+		System.out.println("Do-While döngüsü bitti.");
+			
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
