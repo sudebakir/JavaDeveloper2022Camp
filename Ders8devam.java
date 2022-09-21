f332190abf19bc623a52c2f1617871b9e9052980
sudebakir <81747951+sudebakir@users.noreply.github.com>
2022-09-21 19:17:27 +0300

    Add files via upload

---
+++ Ders8devam.java
@@ -0,0 +1,30 @@
+
+public class Ders8devam {
+
+	public static void main(String[] args) {
+			//for
+		for (int i=2; i<10; i+=2) {
+		
+		System.out.println(i);
+		
+		}
+
+		System.out.println("Döngü Bitti");
+
+		int i=1;
+		    //while
+		while (i<10) {
+			System.out.println(i);
+			i+=2;
+			
+		}
+		
+		
+		
+		System.out.println("Döngü Bitti");
+		
+		
+
+	}
+
+}
